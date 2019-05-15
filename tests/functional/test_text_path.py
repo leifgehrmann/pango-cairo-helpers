e137e9d6e69c800d88bf8b2520344b074198b330
Leif Gehrmann <leif.gehrmann@gmail.com>
2019-05-15 07:32:37 +0100

    Make tests output to multiple files, rather than overwriting the same one.

--- tests/functional/test_text_path.py
+++ tests/functional/test_text_path.py
@@ -16,8 +16,8 @@ class TestTextPath(unittest.TestCase):
         cairo_context = Context(surface)
         return surface, cairo_context
 
-    def _create_real_surface(self) -> Tuple[Surface, Context]:
-        surface = SVGSurface('tests/output/test.svg', 100, 100)
+    def _create_real_surface(self, filename: str) -> Tuple[Surface, Context]:
+        surface = SVGSurface('tests/output/text_path_%s' % filename, 100, 100)
         cairo_context = Context(surface)
         return surface, cairo_context
 
@@ -32,7 +32,7 @@ class TestTextPath(unittest.TestCase):
             TextPath(line_string, layout)
 
     def test_text_fits(self):
-        surface, cairo_context = self._create_real_surface()
+        surface, cairo_context = self._create_real_surface('text_fits.svg')
         layout = pangocairocffi.create_layout(cairo_context)
         layout.set_markup('Hi from Παν語')
 
@@ -45,7 +45,9 @@ class TestTextPath(unittest.TestCase):
         assert not text_path.text_fits()
 
     def test_compute_boundaries(self):
-        surface, cairo_context = self._create_real_surface()
+        surface, cairo_context = self._create_real_surface(
+            'compute_boundaries.svg'
+        )
         layout = pangocairocffi.create_layout(cairo_context)
         layout.set_markup('Hi from Παν語')
 
@@ -53,8 +55,8 @@ class TestTextPath(unittest.TestCase):
         text_path = TextPath(line_string, layout)
         assert text_path.compute_boundaries() is None
 
-    def test_glyph(self):
-        surface, cairo_context = self._create_real_surface()
+    def test_draw(self):
+        surface, cairo_context = self._create_real_surface('draw.svg')
         layout = pangocairocffi.create_layout(cairo_context)
         layout.set_markup('Hi from Παν語')
 
