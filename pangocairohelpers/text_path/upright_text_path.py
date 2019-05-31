974e4abceca02a5428f346cbddf24363cdcb04ab
Leif Gehrmann <leif.gehrmann@gmail.com>
2019-05-31 08:10:16 +0100

    Made UprightTextPath extend TextPathAbstract.
    Still needs implementation, but that required additional work.

--- pangocairohelpers/text_path/upright_text_path.py
+++ pangocairohelpers/text_path/upright_text_path.py
@@ -1,5 +1,25 @@
-class UprightTextPath:
+from typing import Optional
+
+from cairocffi import Context
+from shapely.geometry import MultiPolygon, LineString
+
+from pangocairohelpers.text_path import TextPathAbstract
+
+
+class UprightTextPath(TextPathAbstract):
     """
     A TextPath that when drawn, will always try to make sure the glyphs are
     never rendered upside down for poor readability.
     """
+
+    def text_fits(self) -> bool:
+        pass
+
+    def compute_baseline(self) -> Optional[LineString]:
+        pass
+
+    def compute_boundaries(self) -> MultiPolygon:
+        pass
+
+    def draw(self, context: Context):
+        pass
