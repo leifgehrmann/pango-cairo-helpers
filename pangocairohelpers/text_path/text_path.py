10c7269a39d68fcb336008cd8e1ff12ae7aa6127
Leif Gehrmann <leif.gehrmann@gmail.com>
2019-06-01 11:12:15 +0100

    Added ability to compute baseline

--- pangocairohelpers/text_path/text_path.py
+++ pangocairohelpers/text_path/text_path.py
@@ -1,14 +1,14 @@
-from typing import Optional
+from typing import Optional, List
 
 from cairocffi import Context
 from pangocffi import Layout
 from shapely.geometry import LineString, MultiPolygon
 from pangocairocffi.render_functions import show_glyph_item
 
-from pangocairohelpers import Side
-from pangocairohelpers.line_string_helper import reverse, \
+from pangocairohelpers import Side, point_helper
+from pangocairohelpers.line_string_helper import reverse, substring, \
     parallel_offset_with_matching_direction
-from pangocairohelpers.text_path import TextPathAbstract
+from pangocairohelpers.text_path import TextPathAbstract, TextPathGlyphItem
 
 
 class TextPath(TextPathAbstract):
@@ -35,15 +35,7 @@ class TextPath(TextPathAbstract):
         """
         super().__init__(line_string, layout)
 
-    def text_fits(self) -> bool:
-        """
-        :return:
-            true if all the glyphs can be rendered on the line
-        """
-        text_path_glyph_items = self._compute_text_path_glyph_items()
-        number_of_layed_out_glyphs = len(text_path_glyph_items)
-        number_of_total_glyphs = len(self._layout_clusters.get_clusters())
-        return number_of_layed_out_glyphs == number_of_total_glyphs
+        self._modified_line_string = None  # type: Optional[LineString]
 
     def _generate_modified_line_string(self):
         self._modified_line_string = self._input_line_string
@@ -85,15 +77,36 @@ class TextPath(TextPathAbstract):
         if self._layout_engine.start_offset != self._start_offset:
             self._layout_engine.start_offset = self._start_offset
 
-    def _compute_text_path_glyph_items(self):
+    def _compute_text_path_glyph_items(self) -> List[TextPathGlyphItem]:
         self._generate_layout_engine()
-        if self._text_path_glyph_items is None:
-            self._text_path_glyph_items = self._layout_engine. \
-                generate_text_path_glyph_items()
+        self._text_path_glyph_items = self._layout_engine. \
+            generate_text_path_glyph_items()
         return self._text_path_glyph_items
 
+    def text_fits(self) -> bool:
+        text_path_glyph_items = self._compute_text_path_glyph_items()
+        number_of_laid_out_glyphs = len(text_path_glyph_items)
+        number_of_total_glyphs = len(self._layout_clusters.get_clusters())
+        return number_of_laid_out_glyphs == number_of_total_glyphs
+
     def compute_baseline(self) -> Optional[LineString]:
-        pass
+        text_path_glyph_items = self._compute_text_path_glyph_items()
+        
+        # Get the start position
+        start_point = text_path_glyph_items[0].position
+
+        # Get the end position
+        end_glyph = text_path_glyph_items[-1]
+        end_point = point_helper.add_polar_vector(
+            end_glyph.position,
+            end_glyph.rotation,
+            end_glyph.logical_extent.width
+        )
+
+        start_offset = self._modified_line_string.project(start_point)
+        end_offset = self._modified_line_string.project(end_point)
+
+        return substring(self._modified_line_string, start_offset, end_offset)
 
     def compute_boundaries(self) -> Optional[MultiPolygon]:
         pass
